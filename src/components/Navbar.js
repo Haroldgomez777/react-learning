0e59402c30307b7b7190e1db516f070642a0f375
Harold Gomez <harold.gz@pitsolutions.com>
2024-04-17 10:43:28 +0530

    fileName

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Dropdown from './Dropdown';
+import Dropdown from './DropDown';
 
 const Navbar = () => {
  const [isOpen, setIsOpen] = useState(false);
